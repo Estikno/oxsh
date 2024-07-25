88509d002e91bbb88b285e5307b3db6e9a5259ab
Estikno <95084542+Estikno@users.noreply.github.com>
2024-07-25 09:53:05 +0200

    Separate parsing command into its own function
    I separated the parse command functionality into its own function to have the code cleaner.
    
    Also added some tests to this parse command function.
    
    Added comments to the read_input function.

--- src/prompt.rs
+++ src/prompt.rs
@@ -8,12 +8,26 @@ fn get_prompt() -> String {
     String::from("> ")
 }
 
+
+/// Reads user input from the console using the rustyline library
+///
+/// # Arguments
+///
+/// * `rl` - A mutable reference to the rustyline Editor object
+///
+/// # Returns
+///
+/// * `Result<String, anyhow::Error>` - The user input as a String,
+///   or an error if there was a problem reading the input.
 pub fn read_input(rl: &mut Editor<(), rustyline::history::FileHistory>) -> Result<String> {
+    // Read user input from the console using the rustyline library
     match rl.readline(get_prompt().as_str()) {
+        // If the input was successfully read, add it to the history and return it
         Ok(input) => {
             rl.add_history_entry(input.as_str())?;
             Ok(input)
         },
+        // If there was an error reading the input, print the error and return an empty String
         Err(err) => {
             println!("Error: {:?}", err);
             Ok(String::new())
--- src/shell.rs
+++ src/shell.rs
@@ -1,6 +1,6 @@
 use crate::commands::{cd, help};
 use anyhow::Result;
-use std::process::{Child, Command, Stdio};
+use std::{process::{Child, Command, Stdio}, str::SplitAsciiWhitespace};
 
 pub enum ShellStatus {
     Continue,
@@ -15,14 +15,10 @@ pub fn shell_logic(input: &String) -> Result<ShellStatus> {
     while let Some(command) = commands.next() {
         // everything after the first whitespace character
         //     is interpreted as args to the command
-        let mut parts = command.trim().split_ascii_whitespace();
-        let command = match parts.next() {
-            Some(command) => command,
-            None => {
-                return Ok(ShellStatus::Continue);
-            } // breaks if no command provided
+        let (command, args) = match parse_command(command) {
+            Some((command, args)) => (command, args),
+            None => return Ok(ShellStatus::Continue)
         };
-        let args = parts;
 
         match command {
             "cd" => {
@@ -71,4 +67,68 @@ pub fn shell_logic(input: &String) -> Result<ShellStatus> {
     }
 
     Ok(ShellStatus::Continue)
+}
+
+/// Parses a command from a given input string.
+///
+/// # Arguments
+///
+/// * `input` - The input string to parse the command from.
+///
+/// # Returns
+///
+/// * `Some((command, args))` - A tuple containing the command and the arguments.
+/// * `None` - If no command is provided in the input string.
+fn parse_command(input: &str) -> Option<(&str, SplitAsciiWhitespace)> {
+    // Split the input string into parts separated by whitespace characters.
+    let mut parts = input.trim().split_ascii_whitespace();
+
+    // Get the first part as the command.
+    let command = match parts.next() {
+        Some(command) => command,
+        None => {
+            return None; // Return None if no command is provided.
+        }
+    };
+
+    // Collect the remaining parts as the arguments.
+    let args: SplitAsciiWhitespace = parts;
+
+    // Return the command and arguments as a tuple.
+    Some((command, args))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_command_with_command_and_args() {
+        let input = "ls -l";
+        let expected_command = "ls";
+        let expected_args: Vec<&str> = vec!["-l"];
+
+        let result = parse_command(input).unwrap();
+
+        assert_eq!(result.0, expected_command);
+        assert_eq!(result.1.collect::<Vec<&str>>(), expected_args);
+    }
+
+    #[test]
+    fn test_parse_command_with_command_only() {
+        let input = "ls";
+        let expected_command = "ls";
+        let expected_args: Vec<&str> = vec![];
+
+        let result = parse_command(input).unwrap();
+        
+        assert_eq!(result.0, expected_command);
+        assert_eq!(result.1.collect::<Vec<&str>>(), expected_args);
+    }
+
+    #[test]
+    fn test_parse_command_with_no_command() {
+        let input = "";
+        assert_eq!(parse_command(input).is_none(), true);
+    }
 }
